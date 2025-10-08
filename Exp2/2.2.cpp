01aacc4cad0cfa99630545594e9c030093ec38d7
lakshya goyal (zero) <lakshyagoyal701@gmail.com>
2025-10-08 21:59:16 +0530

    New file

---
+++ Exp2/2.2.cpp
@@ -0,0 +1,34 @@
+// Objective - C++ programs using concept of structure
+// WAP in  C++ enter two student detials with n subject marks using array with-in structure
+#include<iostream>
+using namespace std;
+
+struct student {
+   
+        int rollno;
+        char name[20];
+        float fees;
+        int marks[5];
+    }S;
+
+    int main(){
+    cout << "Enter your roll no\n";
+    cin >> S.rollno; 
+    cout << "Enter your name\n";
+    cin >> S.name; 
+    cout << "Enter your fees\n";
+    cin >> S.fees;
+    cout << "Enter subjects makrs\n";
+    for(int i=0; i<5;i++){
+        cin >> S.marks[i];
+    }
+    
+    cout << "Roll no :"<<S.rollno<<endl;
+    cout << "Name :"<<S.name<<endl;
+    cout << "Fees :"<<S.fees<<endl;
+    cout << "subjects makrs\n";
+    for(int i=0; i<5;i++){
+        cin >> S.marks[i];
+    }
+        return 0;
+    }
